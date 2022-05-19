43f12ffe7ac4a137e109bcb521200ea3671a1a00
KIM HANA <hnone99@gmail.com>
2022-05-19 16:31:36 +0900

    멀티 셀렉트 라이브러리 추가

--- src/assets/js/ui.js
+++ src/assets/js/ui.js
@@ -6,7 +6,28 @@ let Common = {
         $('[data-toggle="right-card"]').on('click', function (e) {
             e.preventDefault();
             $("#right-card").toggleClass("d-none");
+        });
+
+        //var $bsMultiSelects = $('[multiple="multiple"]');
+        $('[multiple="multiple"]').each(function () {
+            let ph = $(this).attr('placeholder')
+            $(this).bsMultiSelect({
+                cssPatch: {
+                    choice: 'p-1',
+                    choices: {
+                        columnCount: "2",
+                    },
+                },
+                css: {
+                    pick: "",
+                    pickButton: "btn-close ms-1",
+                    choiceContent: 'form-check mb-0'
+
+                },
+                placeholder: ph,
+            });
         })
+
     }
 }
 

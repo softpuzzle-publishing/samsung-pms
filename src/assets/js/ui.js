1a9a001460445102bee6589e04dffb204e6f36e1
DESKTOP-F3DS1VQ\publishers <jang0ha@gmail.com>
2022-05-25 14:57:07 +0900

    system/country_modify.html 확인중

--- src/assets/js/ui.js
+++ src/assets/js/ui.js
@@ -3,9 +3,20 @@ let Common = {
         this.event();
     },
     event: function () {
-        $('[data-toggle="right-card"]').on('click', function (e) {
-            e.preventDefault();
-            $("#right-card").toggleClass("d-none");
+        // $('[data-toggle="right-card"]').on('click', function (e) {
+        //     e.preventDefault();
+        //     $("#right-card").toggleClass("d-none");
+        // });
+
+        // 테이블 tr 효과
+        $(".table-hover").on("click", "tr", function () {
+            $(this).toggleClass("tr-active");
+            if ($(this).hasClass("tr-active")) {
+                $(".table-hover tr").not(this).removeClass("tr-active");
+                $("#right-card").removeClass("d-none");
+            } else {
+                $("#right-card").addClass("d-none");
+            }
         });
 
         // 버튼 active
@@ -33,15 +44,15 @@ let Common = {
                     placeholder: ph,
                 });
             });
-        } 
+        }
 
 
         // 데이트피커
-        $( "#before-datepicker, #after-datepicker" ).datepicker();
+        $("#before-datepicker, #after-datepicker").datepicker();
         //
         function readImage(input) {
             // 인풋 태그에 파일이 있는 경우
-            if(input.files && input.files[0]) {
+            if (input.files && input.files[0]) {
                 // 이미지 파일인지 검사 (생략)
                 // FileReader 인스턴스 생성
                 const reader = new FileReader()
@@ -59,8 +70,17 @@ let Common = {
         if (inputImage !== null) {
             inputImage.addEventListener("change", e => {
                 readImage(e.target)
+                //이미지 삭제 버튼
+                $(".image-delete-btn").show();
             });
-        }
+        } 
+        // input file에 이미지 삭제
+        $('.upload-image-wrap').on('click', '.image-delete-btn', function () {
+            $("#preview-image").attr("src", "/assets/images/default-image.png");
+            $("#input-image").val("");
+            $(".image-delete-btn").hide();
+            console.log('55');
+        });
     }
 }
 

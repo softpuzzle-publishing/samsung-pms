faf5aeec35dd212964617572a145226db978cfe9
DESKTOP-F3DS1VQ\publishers <jang0ha@gmail.com>
2022-11-09 12:19:10 +0900

    fix: 가격데이터 수정 / 모달 수정/ css 수정

--- dist/assets/js/ui.js
+++ dist/assets/js/ui.js
@@ -9,10 +9,10 @@ var Common = {
     //     e.preventDefault();
     //     $("#right-card").toggleClass("d-none");
     // });
+
     // 테이블 tr 효과
     $(".table-hover").on("click", "tr", function () {
       $(this).toggleClass("tr-active");
-
       if ($(this).hasClass("tr-active")) {
         $(".table-hover tr").not(this).removeClass("tr-active");
         $("#right-card").removeClass("d-none");
@@ -22,17 +22,19 @@ var Common = {
     });
     $('.tab-close').on('click', function () {
       $("#right-card").addClass("d-none");
-    }); // 버튼 active
-
+    });
+    // 버튼 active
     $('[data-toggle="right-card"]').on('click', function (e) {
       $("#right-card").toggleClass("d-none");
-    }); // 버튼 active
+    });
 
+    // 버튼 active
     $('[data-toggle="btn-select"]').on('click', function (e) {
       e.preventDefault();
       $(this).toggleClass("active");
-    }); // 가격데이터 셀렉박스 옵션선택
+    });
 
+    // 가격데이터 셀렉박스 옵션선택
     if ($('[multiple="multiple"]').length > 0) {
       $('[multiple="multiple"]').each(function () {
         var ph = $(this).attr('placeholder');
@@ -47,19 +49,20 @@ var Common = {
             pick: "selected",
             pickButton: "btn-close ms-1",
             choiceContent: 'form-check mb-0'
-          } // placeholder: ph,
-
+          }
+          // placeholder: ph,
         });
+
         $(".form-select.custom[placeholder='" + ph + "']").next().find('ul.form-control').prepend("<li class=\"placeholder-wrap\">" + ph + "</li>");
       });
-    } // 데이트피커
+    }
+
+    // 데이트피커
     // $("#before-datepicker, #after-datepicker").datepicker({
     //     dateFormat: "yy.mm.dd",
     //     showButtonPanel: true,
     //     currentText: "Now",
     // }).datepicker('setDate', 'today');
-
-
     $("#before-datepicker, #after-datepicker").datetimepicker({
       format: "yyyy-mm-dd",
       minDate: '0',
@@ -83,43 +86,40 @@ var Common = {
       beforeShow: function beforeShow(input, inst) {
         $('#ui-datepicker-div').addClass('datepicker-wrapper');
       }
-    }); //
+    });
 
+    //
     function readImage(input) {
       // 인풋 태그에 파일이 있는 경우
       if (input.files && input.files[0]) {
         // 이미지 파일인지 검사 (생략)
         // FileReader 인스턴스 생성
-        var reader = new FileReader(); // 이미지가 로드가 된 경우
-
+        var reader = new FileReader();
+        // 이미지가 로드가 된 경우
         reader.onload = function (e) {
           var previewImage = document.getElementById("preview-image");
           previewImage.src = e.target.result;
-        }; // reader가 이미지 읽도록 하기
-
-
+        };
+        // reader가 이미지 읽도록 하기
         reader.readAsDataURL(input.files[0]);
       }
-    } // input file에 change 이벤트 부여
-
-
+    }
+    // input file에 change 이벤트 부여
     var inputImage = document.getElementById("input-image");
-
     if (inputImage !== null) {
       inputImage.addEventListener("change", function (e) {
-        readImage(e.target); //이미지 삭제 버튼
-
+        readImage(e.target);
+        //이미지 삭제 버튼
         $(".image-delete-btn").show();
       });
-    } // input file에 이미지 삭제
-
-
+    }
+    // input file에 이미지 삭제
     $('.upload-image-wrap').on('click', '.image-delete-btn', function () {
       $("#preview-image").attr("src", "/assets/images/default-image.png");
       $("#input-image").val("");
       $(".image-delete-btn").hide();
-    }); // 
-
+    });
+    // 
     $('.has-treeview > a').click(function () {
       $(this).parent().toggleClass('hide');
     });
